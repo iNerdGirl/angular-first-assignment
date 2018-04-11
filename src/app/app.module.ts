f65df0642a2fe6a16afaaa57fcdde094ea3e699c
Autumn Raine <autumn.raine@icloud.com>
2018-04-11 11:45:49 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SuccessMessageComponent } from './success-message/success-message.component';
+import { FailMessageComponent } from './success-message/fail-message/fail-message.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessMessageComponent,
+    FailMessageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/success-message/fail-message/fail-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-fail-message',
+  templateUrl: './fail-message.component.html',
+  styleUrls: ['./fail-message.component.css']
+
+})
+
+export class FailMessageComponent {
+
+}
